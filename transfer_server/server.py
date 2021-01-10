bde3f830cc404d947307eb134e0ae4604be79f35
Lino Steinhau <lino.steinhau@fu-berlin.de>
2021-01-10 23:29:52 +0100

    added transfer server

---
+++ transfer_server/server.py
@@ -0,0 +1,23 @@
+import socket
+import requests
+
+UDP_IP = "::"
+UDP_PORT = 12345
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+sock.bind((UDP_IP,UDP_PORT))
+
+while True:
+  d = ''
+  while True:
+    data, addr = sock.recvfrom(8192)
+    print("received: ",len(data))
+    s = data.decode('utf-8')
+    if s.startswith("filename="):
+      filename = s[9:]
+      continue
+    if s == "close":
+      break
+    d += s
+  requests.post('http://192.168.178.32:5080/api/upload-cwa-data/'+filename, data=d)
+
+sock.close()
